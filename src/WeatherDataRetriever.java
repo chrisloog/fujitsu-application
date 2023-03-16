9216cc6aeec5fa67542e22e807ae140b1820363d
Chris <chrisloog120@gmail.com>
2023-03-16 18:03:56 +0200

    Add connection to database

---
+++ src/WeatherDataRetriever.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class WeatherDataRetriever {
+
+    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/your-database-name";
+    private static final String USERNAME = "your-username";
+    private static final String PASSWORD = "your-password";
+
+    public HashMap<String, Object> getWeatherDataByCityAndDate(String city, LocalDate date) throws SQLException {
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+
+        String query = "SELECT airTemp, windSpeed, weatherPhenomenon FROM WEATHER_AT WHERE cityName = ? AND timeStamp::date = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, city);
+        statement.setObject(2, date != null ? date : LocalDate.now());
+
+        ResultSet resultSet = statement.executeQuery();
+
+        HashMap<String, Object> weatherData = new HashMap<>();
+        if (resultSet.next()) {
+            weatherData.put("airTemp", resultSet.getDouble("airTemp"));
+            weatherData.put("windSpeed", resultSet.getDouble("windSpeed"));
+            weatherData.put("weatherPhenomenon", resultSet.getString("weatherPhenomenon"));
+        }
+
+        connection.close();
+
+        return weatherData;
+    }
+
+    public HashMap<String, Object> getWeatherDataByCity(String city) throws SQLException {
+        return getWeatherDataByCityAndDate(city, null);
+    }
+}
